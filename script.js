b13e143de08e180916c08c2ee167ac77b536cffa
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-09-21 08:41:32 -0700

    added googlemaps iframe with location address

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-/* Google map
-------------------------------------------------*/
-var map = '';
-var center;
-
-function initialize() {
-    var mapOptions = {
-        zoom: 16,
-        center: new google.maps.LatLng(37.769725, -122.462154),
-        scrollwheel: false
-    };
-
-    map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-
-    google.maps.event.addDomListener(map, 'idle', function () {
-        calculateCenter();
-    });
-
-    google.maps.event.addDomListener(window, 'resize', function () {
-        map.setCenter(center);
-    });
-}
-
-function calculateCenter() {
-    center = map.getCenter();
-}
-
-function loadGoogleMap() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
-    document.body.appendChild(script);
-}
-
 function setNavbar() {
     if ($(document).scrollTop() > 160) {
         $('.sidebar').addClass('sticky');
